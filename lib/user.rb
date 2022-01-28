b41952b490aec697f9784016191eed968ce5d025
Luke Blackburn <75164517+blue-luke@users.noreply.github.com>
2022-01-28 22:41:38 +0000

    Have isolated test for password unlocking notelist. Also hashed out password tests, rather than x'd out, as it keeps rspec cleaner

--- lib/user.rb
+++ lib/user.rb
@@ -1,12 +1,20 @@
 class User
   attr_reader :name
-  def initialize(name, password)
+  def initialize(name, password, notelist = Notelist)
     @name = name
     @password = password
+    @notelist = notelist.new
+    @unlocked = false
   end
 
   def check_password(entered_password)
     @password == entered_password
+    @unlocked = true
+  end
+
+  def notelist
+    raise "User is locked" unless @unlocked
+    @notelist
   end
 
 end 
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -5,12 +5,19 @@ describe 'User' do
     u = User.new("James", "123456")
     expect(u.name).to eq "James"
   end
-  it 'has a password' do
-    u = User.new("James", "123456")
-    expect(u.password).to eq "123456"
-  end
-  xit 'can check an entered password matches actual password' do
-    u = User.new("James", "123456")
-    expect(u.check_password("123457")).to eq false
+  # it 'has a password' do
+  #   u = User.new("James", "123456")
+  #   expect(u.password).to eq "123456"
+  # end
+  # it 'can check an entered password matches actual password' do
+  #   u = User.new("James", "123456")
+  #   expect(u.check_password("123457")).to eq false
+  # end
+  it 'entering correct password unlocks notelist' do
+    notelist_double = double("notelist")
+    notelist_class_double = double("Notelist", :new => notelist_double)
+    u = User.new("James", "123456", notelist_class_double)
+    u.check_password("123456")
+    expect(u.notelist).to eq notelist_double
   end
 end
